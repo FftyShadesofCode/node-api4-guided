87df37b42c85578c33a5753715d8f4c1c930d9b3
FftyShadesofCode <philiprmcdavid@gmail.com>
2022-01-13 13:20:13 -0500

    changing to main

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+// const cohort1 = process.argv[2]
+// const user = process.env.USER
+// const shell = process.env.SHELL
+//
+// if (cohort1 === 'web-49') {
+//     console.log(`${cohort1} is the best forever!`)
+// } else {
+//     console.log(`booooh cohort ${cohort1}`)
+// }
+//
+// console.log(`the user is ${user}`)
+// console.log(`the shell is ${shell}`)
+
+const express = require('express')
+const app = express()
+
+app.get('hello', (req, res) => {
+    res.json({ message: 'hey there' })
+})
+
+const port = process.env.PORT || 9000
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
